e6a2dc277c8357c0b71ad434b8e5d571d640759b
mv <mivogelpohl@gmail.com>
2013-09-10 14:57:24 +0200

    file

---
+++ Temperature.py
@@ -0,0 +1,65 @@
+import serial
+import sys
+import time
+#test
+#test1
+#test2
+#test3
+ser = serial.Serial()
+ser.baudrate = 9600
+ser.port = "/dev/ttyACM0" #linux
+
+def setupSerial():
+	while True:
+		try:
+			ser.open()
+			break
+		except:
+			print("could not open, trying again")
+			time.sleep(1)
+			try:
+				ser.close()
+			except:
+				pass
+
+# def setupFile(name):
+# 	try:
+# 		f = open(name, 'w')
+# 		return f
+# 	except:
+# 		print("Could not open txt file")
+# 		ser.close()
+# 		sys.exit()
+
+def callArduino():
+	ser.flushInput()
+	if ser.read(1)!='A':
+		print("Arduino not ready, exiting...")
+		ser.close()
+		sys.exit()
+	else:
+		print("calling...")
+		ser.write("calling")
+		ser.flushInput()
+		if ser.read(1)=='A':
+			print("Arduino did not respond, exiting...")
+			#sys.exit()
+		else:
+			print("Call succesfull.")
+
+def closeAll():
+	ser.close()
+	f.close()
+####################################################################################
+
+setupSerial()
+#setupFile("data.txt")
+callArduino()
+f = open("data.txt",'w')
+for i in range(11):
+	ser.flushInput()
+	print(str(ser.read(4)))
+closeAll()
+# ser.close()
+# f.close()
+# print("Done.")
